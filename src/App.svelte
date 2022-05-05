368f58dcb80ec4458b94707c317042bf7237b31c
Tyler Toomey <toomey@rowan.edu>
2022-05-04 20:58:46 -0400

    Animations galore

--- src/App.svelte
+++ src/App.svelte
@@ -1,43 +1,64 @@
-
 <script>
-	import { onMount } from 'svelte'
-	import { slide } from 'svelte/transition'
+  import { onMount } from 'svelte'
+  import { slide, fade } from 'svelte/transition'
 
-	import Search from './components/Search.svelte'
-	import Table from './components/Table.svelte'
+  import Card from './components/Card.svelte'
 
-	import { getSummonerInformation, getMastery } from './utils'
-
-	let data = [];
-	const handleSearch = async function(e){	
-		let {region, summonerName} = e.detail;
-		let summonerInfo = await getSummonerInformation(region,summonerName);
-		data = await getMastery(summonerInfo?.id)
-		console.log(data)
-	}
-
-	onMount(async () => {
-	})
-
+  let loaded = false;
+  onMount(async () => {
+    loaded = true;
+  })
 </script>
 
-<div class="flex items-center justify-center w-full h-1/2 mx-auto">
-	<Search on:searchEvent={handleSearch}/>
-</div>
+<main>
+  {#if loaded}
+    <div class="container box-border flex mx-auto py-8 mt-4 sm:w-[1024px] max-w-5xl content-center rounded-md">
+      <div class="text-4xl text-orange-600 nameplate p-0"in:fade={{duration:1000,delay:500}}>TYLER TOOMEY</div>
+      <div class="ml-3 my-auto text-gray-600"in:fade={{duration:1000,delay:800}}>WebDev,&nbsp;</div>
+      <div class="my-auto text-gray-600"in:fade={{duration:1000,delay:1400}}>Design,&nbsp;</div>
+      <div class="my-auto text-gray-600"in:fade={{duration:1000,delay:2000}}> & More</div>
 
-{#if data}
-<div class="flex items-center justify-center w-full h-1/2 mx-auto" transition:slide={{duration:500}}>
-	<Table {data}/>
-</div>
-{/if}
+    </div>
+    <div class="container box-border relative flex mx-auto px-4 py-8 pt-10 mt-4 sm:w-[1024px] max-w-5xl content-center rounded-md border-4 border-orange-600">
+      <span class="absolute left-3 -top-3 bg-white mx-4 px-2 projects">PROJECTS</span>
+      <Card 
+      name={"SOFDLE"} 
+      description={"SOFDLE is a wordle-like game. SOFDLE was made as a marketing tool for a musician's new single and was made using Svelte and TailwindCSS"}
+      image={"./assets/sofdle.png"}></Card>
+      <Card></Card>
+    </div>
+  {/if}
+
+
+</main>	
 
 <style lang="postcss" global>
   @tailwind base;
   @tailwind components;
   @tailwind utilities;
 
+  .nameplate {
+    font-family: 'Comfortaa';
+    padding:0;
+    text-align:center;
+    margin:auto;
+  }
+
+  .projects {
+    font-family: 'Comfortaa';
+    text-align:center;
+  }
+
+  @media (min-width: 768px){
+    .nameplate {
+      margin:0;
+      text-align:left;
+    }
+  }
+
   /* body{
 	  padding:0px;
   } */
 </style>
 	
+
---
+++ src/components/Card.svelte
@@ -0,0 +1,30 @@
+<script>
+    export let name = "";
+    export let description ="";
+    export let image = "";
+</script>
+
+
+<div class="basis-2/4 mx-4 bg-slate-50 group rounded-md border shadow-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 hover:shadow-xl">
+
+    <a href="https://sofdle.sofmusic.com" target="_blank">
+        <img class="relative rounded-lg w-full p-3 grayscale transition ease-in-out delay-150 group-hover:grayscale-0 duration-300 pb-0 z-50 " src={image} alt="" />
+    </a>
+    <div class="relative flex justify-end pt-0 mt-0 pr-3 -z-50 transition -translate-y-7 duration-200 delay-400 group-hover:translate-y-0 group-hover:opacity-100">
+        <div class="flex w-12 h-6 items-center justify-start bg-slate-600 rounded-b-md ml-1 text-xs text-white">
+            <span class="ml-1">LIVE</span>
+
+            <span class="animate-ping relative inline-flex h-2 w-2 my-auto ml-1 rounded-full bg-orange-600 opacity-75"></span>
+            <span class="relative inline-flex rounded-full h-2 w-2 my-auto -ml-2 bg-orange-600 text-white text-center"></span>
+        </div>
+    </div>
+    <div class="p-5 pt-0">
+        <a href="https://sofdle.sofmusic.com" target="_blank">
+            <div class="mb-2 text-2xl flex font-bold tracking-tight text-orange-600 w-40 m-0 pt-0">{name}
+               
+            </div>
+           
+        </a>
+        <p class="font-normal text-slate-900">{description}</p>
+    </div>
+</div>
